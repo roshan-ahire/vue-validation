779a29d00fa76fd7275a254efe4166017f91e29c
Roshan <roshan.ahire@wohlig.com>
2019-09-06 14:30:39 +0530

    new project created and background add

--- src/main.js
+++ src/main.js
@@ -3,6 +3,34 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// BootstrapVue
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+
+// vue select
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+Vue.component("v-select", vSelect);
+
+//FontAwesomeIcon
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faStar, faCalendar } from "@fortawesome/free-regular-svg-icons"; //far
+import {
+  faChevronRight,
+  faChevronLeft,
+  faTrashAlt
+} from "@fortawesome/free-solid-svg-icons"; //fas
+
+library.add(faCoffee);
+library.add(faStar, faCalendar); //far
+library.add(faChevronRight, faChevronLeft, faTrashAlt); //fas
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/ValidationPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="main-background">
+      <!--  -->
+      <Header></Header>
+      <!--  -->
+    </div>
+  </div>
+</template>
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+<style lang="scss">
+.main-background {
+  background-image: linear-gradient(
+      rgba(223, 39, 230, 0.767),
+      rgba(23, 39, 185, 0.755)
+    ),
+    url("~@/assets/computer.jpg");
+  height: 100vh;
+
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
